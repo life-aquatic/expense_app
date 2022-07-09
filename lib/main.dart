f2d019dc790e212886f7c290ccbf00f00db4a1b0
life-aquatic <f.a.nikitin@gmail.com>
2022-07-09 15:22:20 +0400

    added basic markup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'transaction.dart';
 
 void main() => runApp(ExpApp());
 
@@ -13,13 +15,44 @@ class ExpApp extends StatelessWidget {
 }
 
 class ExpHomepage extends StatelessWidget {
+  List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 43.33,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Food',
+      amount: 12.33,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("vasya"),
       ),
-      body: Text("ahaha"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Card(
+            child: Container(
+              width: 299,
+              child: Text("summary"),
+            ),
+            elevation: 5,
+          ),
+          Card(
+            color: Colors.amber,
+            child: Text('list of tx'),
+          ),
+        ],
+      ),
     );
   }
 }
